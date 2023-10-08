9e69a17737f4cd796c3793e20a4229ca39690b6c
Ekta Ramavatar Sharma <ektar@boltinc.com>
2023-10-08 15:42:11 +0530

    Assignment_ Numberpower

---
+++ src/NumberPower.java
@@ -0,0 +1,31 @@
+
+
+public class NumberPower {
+
+	public static void main(String[] args) {
+		
+		
+		int number= 2;
+		
+		 System.out.println("Input number is : " +number);
+		
+		//Square
+		
+		int square= (number * number);
+		 System.out.println(" Square :"+square);
+		 
+		 
+		//cube
+		 
+		 double  cube= Math.pow(number, 3);
+		 System.out.println(" Cube :"+cube);
+		 
+		 
+		 //fourthpower
+		 double fourthpower= Math.pow(number, 4);
+		 System.out.println(" FourthPower :"+fourthpower);
+		
+
+	}
+
+}
