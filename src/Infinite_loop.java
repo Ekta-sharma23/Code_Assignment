65dd2cf8e49524a3bbb6040c0164f87e54dd3208
Ekta Ramavatar Sharma <ektar@boltinc.com>
2023-10-13 19:21:45 +0530

    infinite loop

---
+++ src/Infinite_loop.java
@@ -0,0 +1,19 @@
+
+public class Infinite_loop {
+
+	public static void main(String[] args) {
+	
+		/*
+		 * while (true) { System.out.println("test"); } }
+		 */
+		
+		/*
+		 * for (;;) { System.out.println("test"); }
+		 */
+		
+		do {
+			 System.out.println("test");
+		} while (true);
+	}
+
+}
