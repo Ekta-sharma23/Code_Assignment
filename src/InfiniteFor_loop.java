00172c58b8ac1413bb015538a2f0970abc1aed13
Ekta Ramavatar Sharma <ektar@boltinc.com>
2023-11-24 13:56:21 +0530

    2 ways to write infinite for loop

---
+++ src/InfiniteFor_loop.java
@@ -0,0 +1,21 @@
+
+public class InfiniteFor_loop {
+
+	public static void main(String[] args) {
+
+
+//		boolean i=true;
+//
+//		for(;i;)
+//		{
+//			System.out.println("Test");
+//		}
+		for(;;)
+		{
+			System.out.println("Test");
+		}
+
+	}
+}
+
+
