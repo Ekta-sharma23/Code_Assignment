7b32ebb9caaf8bcb32f691abf475de810794f7ff
Ekta Ramavatar Sharma <ektar@boltinc.com>
2023-11-24 14:43:45 +0530

    Operators17

---
+++ src/Operators17.java
@@ -0,0 +1,44 @@
+
+public class Operators17 {
+
+	public static void main(String[] args) {
+		
+		int result=((6 - 1) * 2) + 7;
+		System.out.println(result);
+		int result1=2*6+7-1;
+		System.out.println(result1);
+		int result2=6*2-1+7;
+		System.out.println(result2);
+		int result3=6-1+7*2;
+		System.out.println(result3);
+		int result4=7+6*2-1;
+		System.out.println(result4);
+		int result5=(2+6)*2-1;
+		System.out.println(result5);
+		int result6=7+6-1*2;
+		System.out.println(result6);
+		int result7=6+2*7-1;
+		System.out.println(result7);
+		int result8=(7-1)*2+6;
+		System.out.println(result8);
+		int result9=2*7+6-1;
+		System.out.println(result9);
+		int result10=(2-1)*6+7;
+		System.out.println(result10);
+		int result11=1-7*6+2;
+		System.out.println(result11);
+		int result12=7*2-1+6;
+		System.out.println(result12);
+		int result13=2*7-1+6;
+		System.out.println(result13);
+		int result14=(1+7)*2-6;
+		System.out.println(result14);
+		int result15=(6-1)*(2+7);
+		System.out.println(result15);
+		int result16=((6 - 1) * 2) + 7;
+		System.out.println(result16);
+
+
+	}
+
+}
