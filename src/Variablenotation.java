ed6b70d5452f58931ad4bbf5443f20dd2c9e28ed
Ekta Ramavatar Sharma <ektar@boltinc.com>
2023-11-25 14:44:00 +0530

    variable notation is correct or not

---
+++ src/Variablenotation.java
@@ -0,0 +1,15 @@
+
+public class Variablenotation {
+
+	public static void main(String[] args) {
+		int i = 10;  //CORRECT
+//		int int = 20; 
+//		int goto = 30; 
+//		int default = 40; 
+//		int class = 50; 
+		int j = 70;//CORRECT
+		
+		//Rest are incorrect because  the variable names int, goto, default, and class are reserved keyword.
+	}
+
+}
