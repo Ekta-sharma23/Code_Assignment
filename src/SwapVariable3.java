5e7257f818e53cf3201d46e656a2f2098c4d7f94
Ekta Ramavatar Sharma <ektar@boltinc.com>
2023-11-25 15:20:08 +0530

    swap values using third variable

---
+++ src/SwapVariable3.java
@@ -0,0 +1,21 @@
+
+public class SwapVariable3 {
+
+	public static void main(String[] args) {
+	
+		 int i = 10;
+		    int j = 7;
+		    int temp;
+		   
+		    {
+		    	temp=i;
+		    	i=j;
+		    	j=temp;
+		    	
+		    	
+		    }
+		    System.out.println(i);
+		    System.out.println(j);
+	}
+
+}
